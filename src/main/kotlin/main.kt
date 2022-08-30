55f1207daa4e8897d359c645655015156ccb62c9
Norman Pendzich <norman.pendzich@outlook.de>
2022-08-30 06:41:21 +0200

    23 commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
 
     var x = 3
@@ -17,4 +19,24 @@ fun main(args: Array<String>) {
         println("x is now $x")
 
     }
+    println("--------------------")
+
+    var y = 4
+    while (y > 3) {
+        println(y)
+        y -= 1
+    }
+
+    val a = 3
+    val b = 1
+    if (a > b) {
+        println("a is greater then b")
+    } else {
+        println("a is not greater then b")
+    }
+    println("this lines run no matter what")
+    println(if(a>b) "a is greater then b" else "a is not greater then b")
+    if(a>b) "a is greater then b" else "a is not greater then b"
+
+
 }
